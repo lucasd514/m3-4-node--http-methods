70ee5d33c733a229cda49a263c9941dbabc3f634
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-07 17:26:36 -0400

    variables defined

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { stock, customers } = require("../data/promo");
+const { stock, customers } = require("./data/promo");
 express()
   .use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,14 +19,45 @@ express()
   .use(express.urlencoded({ extended: false }))
   .set("view engine", "ejs")
   .post("/order", (req, res) => {
-    const newOrder = req.body;
-    const stockLevels = req.stock;
-    console.log(stockLevels);
-    if (newOrder.country === "Canada") {
-      console.log("good");
-      res.json({ status: "success" });
+    console.log(req.body);
+    //variables
+    const newOrder = req.body.order;
+    console.log(newOrder);
+    const tshirtSize = req.body.size;
+    const tshirtSizeCheck = stock.shirt;
+    const sizeStock = tshirtSizeCheck[tshirtSize];
+    const itemStock = stock[newOrder];
+    const shipToCountry = req.body.country;
+
+    console.log(tshirtSize);
+    console.log(sizeStock);
+    console.log(stock.shirt.small);
+    //shirt conditions
+    let inStock = () => {
+      if (newOrder === "shirt") {
+        if (sizeStock > 0) {
+          console.log("gotcha");
+        } else {
+          console.log("not enough fam");
+          res.json({ status: "error", error: "unavailable" });
+        }
+        // res.json({ status: "success" });
+      } else {
+        if (itemStock > 0) {
+          console.log("yerrrr");
+        } else {
+          console.log("not enough fam");
+          res.json({ status: "error", error: "unavailable" });
+        }
+      }
+    };
+    //country
+    const countryShip = req.body.country;
+    console.log(countryShip);
+    if (shipToCountry !== "Canada") {
+      res.json({ status: "error", error: "undeliverable" });
     } else {
-      console.log("wrong country");
+      inStock();
     }
   })
   // endpoints
